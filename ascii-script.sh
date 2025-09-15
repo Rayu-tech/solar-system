45a9b37e5cbe1d2366a1cfca72c2006b0988576a
Rayu-tech <rayu30616@gmail.com>
2025-09-15 14:42:26 +0530

    updates

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,6 @@
-#/bin/sh
-sudo apt-get install cowsay -y
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y cowsay
 cowsay -f dragon "Run for cover, I am a DRAGOOONNNN...... RAQWRRRRRRRR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
